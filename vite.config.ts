a22a1361ec16c41c175b2370c2bc0dc93508cf13
D5v4d <166731621+D5v4d@users.noreply.github.com>
2025-04-15 16:26:20 +0300

    коментарий

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
   base: '/air-ticket-search/', // Укажите имя вашего репозитория
   build: {
-    outDir: 'dist', // Убедитесь, что это правильная директория сборки
+    outDir: 'docs', // Изменили на docs для GitHub Pages
   },
 });
